c73c763a08c279b8975de1680639fed93108bfe1
Andreea <andreea_popa22@yahoo.ro>
2021-12-06 21:12:10 +0200

    new project

---
+++ src/test/java/com/agira/Agira/AgiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agira.Agira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
