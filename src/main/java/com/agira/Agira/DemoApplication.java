308e8d535c001f7ccc2c400207be0b894b3fc697
alina0311 <61750022+alina0311@users.noreply.github.com>
2022-01-19 21:05:41 +0200

    incercare jpa si spring boot auth

--- src/main/java/com/agira/Agira/DemoApplication.java
+++ src/main/java/com/agira/Agira/DemoApplication.java
@@ -1,20 +1,32 @@
 
 package com.agira.Agira;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @GetMapping("")
+    public String viewHomePage() {
+        return "index";
     }
 
     @GetMapping("/hello")
@@ -40,5 +52,39 @@ public class DemoApplication {
 
         return String.format("Hello %s!", name);
     }
+
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+
+        return "signup_form";
+    }
+
+
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+
+        userRepo.save(user);
+
+        return "register_succ";
+    }
+
+    @GetMapping("/users")
+    public String listUsers(Model model) {
+        List<User> listUsers = userRepo.findAll();
+        model.addAttribute("listUsers", listUsers);
+
+        return "users";
+    }
+
+
+
 }
+
+// http://localhost:8080/hello
             
