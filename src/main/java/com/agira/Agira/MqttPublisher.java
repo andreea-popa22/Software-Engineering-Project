ddd5cb86165e7cda7074110fcebd27e4b8af428a
Andreea <andreea_popa22@yahoo.ro>
2022-01-20 17:37:21 +0200

    try publish and subscribe

---
+++ src/main/java/com/agira/Agira/MqttPublisher.java
@@ -0,0 +1,48 @@
+package com.agira.Agira;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class MqttPublisher {
+
+    public static void main(String[] args) {
+
+    	//Topic name 
+        String topic        = "test";
+        //data to be send
+        String content      = "Temp:20,Humi:70";
+        int qos             = 0;
+        /*hostname is localhost as mqtt publisher and broker are
+          running on the same computer*/ 
+        String broker       = "tcp://localhost:1883";
+        String clientId     = "JavaSample";
+        MemoryPersistence persistence = new MemoryPersistence();
+
+        try {
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            System.out.println("Connecting to broker: "+broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected to broker");
+            System.out.println("Publishing message:"+content);
+            MqttMessage message = new MqttMessage(content.getBytes());
+            message.setQos(qos);
+            sampleClient.publish(topic, message);
+            System.out.println("Message published");
+            sampleClient.disconnect();
+            sampleClient.close();
+            System.exit(0);
+            } catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
+    }
+}
